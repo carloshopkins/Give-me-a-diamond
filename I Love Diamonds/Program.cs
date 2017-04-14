891e93644458c6cb755f2999acb332f78c5f5e6b
Carlos Hopkins <Carlos Hopkins>
2017-04-14 13:41:58 -0400

    Add project files.

---
+++ I Love Diamonds/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_Love_Diamonds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How big do you want your diamond??");
+            Console.WriteLine("Input a number 3 or larger that is odd");
+            var input = int.Parse(Console.ReadLine());
+            Console.WriteLine(PrintDiamond(input));
+        }
+        public static string PrintDiamond(int n)
+        {
+            if (n % 2 == 0 || n < 0)
+                return null;
+
+            StringBuilder result = new StringBuilder();
+
+            var asteriskCount = 1;
+            var whiteSpaceCount = n / 2;
+
+            while (asteriskCount <= n)
+            {
+                for (int i = whiteSpaceCount; i > 0; i--)
+                {
+                    result.Append(" ");
+                }
+                whiteSpaceCount--;
+                for (int i = 0; i < asteriskCount; i++)
+                {
+                    result.Append("*");
+                }
+                result.Append("\n");
+                asteriskCount += 2;
+            }
+            asteriskCount -= 2;
+            whiteSpaceCount = 1;
+            while (asteriskCount > 2)
+            {
+                for (int i = whiteSpaceCount; i > 0; i--)
+                {
+                    result.Append(" ");
+                }
+                whiteSpaceCount++;
+                asteriskCount -= 2;
+                for (int i = asteriskCount; i >= 1; i--)
+                {
+                    result.Append("*");
+                }
+                result.Append("\n");
+
+            }
+
+            return result.ToString();
+
+
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,25 @@
+This kata is to practice simple string output. 
+Jamie is a programmer, and James' girlfriend. 
+She likes diamonds, and wants a diamond string 
+from James. Since James doesn't know how to make 
+this happen, he needs your help.
+
+Task:
+
+You need to return a string that displays a 
+diamond shape on the screen using asterisk 
+("*") characters. Please see provided test 
+cases for exact output format.
+
+The shape that will be returned from print 
+method resembles a diamond, where the number
+ provided as input represents the number of *’s 
+printed on the middle line. The line above and 
+below will be centered and will have 2 less *’s 
+than the middle line. This reduction by 2 *’s for 
+each line continues until a line with a single * 
+is printed at the top and bottom of the figure.
+
+Return null if input is even number or negative 
+(as it is not possible to print diamond with even 
+number or negative number).
